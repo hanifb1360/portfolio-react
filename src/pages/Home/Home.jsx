15d280c68185571a3182fcd4294f2021fee85fd5
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-03-19 20:02:53 +0100

    style modified based on feedback

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -30,43 +30,22 @@ const Home = () => {
       <h2 style={{ transform: `translateY(${scrollPosition * -0.3}px)` }}>
         Front-End Developer
       </h2>
+      <h4 style={{ transform: `translateY(${scrollPosition * -0.5}px)` }}>
+        With a background in Design
+      </h4>
 
       <div className='aboutTextBox'>
         <p className='aboutText'>
-          Hi there! My name is Hanif Bahari, and I'm excited to tell you a bit
-          about myself. First and foremost, I'm a very friendly and approachable
-          person, so feel free to say hello anytime! I believe that making
-          connections with others is one of the most important things we can do
-          in life.
-        </p>
-        <p className='aboutTextInside'>
-          Professionally, I'm a frontend developer with a background in design.
-          I absolutely love creating beautiful, user-friendly interfaces that
-          make people's lives easier. I've worked with a variety of
-          technologies, including React JS, Next JS, and Gatsby JS, as well as
-          GraphQL and REST API. I'm also very comfortable with styling
+          Hi, I'm Hanif Bahari! I'm a friendly and approachable frontend
+          developer with a design background. I enjoy using technologies such as
+          React JS, Next JS, and Gatsby JS, and I'm comfortable with styling
           frameworks like Sass, Chakra UI, Bootstrap, Tailwind CSS, and Styled
-          Components.
-        </p>
-        <p className='aboutTextInside'>
-          I'm passionate about learning and staying up-to-date with the latest
-          trends and technologies. In fact, I'm always seeking out new courses,
-          tutorials, and conferences to attend so I can keep growing as a
-          developer. I believe that the more we learn, the more we can
-          contribute to the world around us.
-        </p>
-        <p className='aboutTextInside'>
-          When I'm not coding, you can often find me drawing and creating art.
-          Illustration and cartooning are a big part of my life, and I find that
-          they help me stay creative and inspired. In fact, I believe that my
-          background in design has given me a unique perspective on frontend
-          development and has helped me create even better user experiences.
-        </p>
-        <p className='aboutTextInside'>
-          Overall, I'm someone who loves to connect with others, create
-          beautiful and user-friendly interfaces, and learn as much as I can.
-          I'm excited about what the future holds, and I look forward to making
-          meaningful connections with others in the industry.
+          Components. I'm passionate about staying up-to-date with the latest
+          trends and technologies and enjoy attending courses, tutorials, and
+          conferences. In my free time, I like to draw and create art. My design
+          background gives me a unique perspective on frontend development and
+          helps me create better user experiences. Let's connect and make
+          meaningful connections in the industry!
         </p>
       </div>
 
