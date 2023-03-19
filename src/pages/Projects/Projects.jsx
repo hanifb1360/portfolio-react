39c577ccce8ccb4e65818874b5f034989bd5685c
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-03-19 10:17:08 +0100

    portfolio code

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -4,10 +4,18 @@ import styles from "./Projects.module.scss";
 
 const Projects = () => {
   return (
-    <div className={styles.projectsContainer}>
-      {projectList.map((project) => (
-        <Project key={project.id} project={project} />
-      ))}
+    <div className={styles.projectsPage}>
+      <div className={styles.portfolioCodeContainer}>
+        <a href='https://github.com/hanifb1360/portfolio-react'>
+          The code behind this website
+        </a>
+      </div>
+
+      <div className={styles.projectsContainer}>
+        {projectList.map((project) => (
+          <Project key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
