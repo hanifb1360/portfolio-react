8bc4706a6a981c4d10362314ca4b852cb44a8469
Hanif Bahari <hanif.bahari@gmail.com>
2023-04-04 15:01:51 +0200

    added two new skills

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -22,6 +22,7 @@ const Skills = () => {
         <SkillBar skill="JavaScript" percentage={70} />
         <SkillBar skill="React" percentage={70} />
         <SkillBar skill="Node.js" percentage={60} />
+        <SkillBar skill="GraphQL" percentage={80} />
         <SkillBar skill="TypeScript" percentage={50} />
         <SkillBar skill="TailwindCSS" percentage={90} />
         <SkillBar skill="Firebase" percentage={70} />
@@ -31,7 +32,8 @@ const Skills = () => {
         <SkillBar skill="Next JS" percentage={70} />
         <SkillBar skill="StoryBook" percentage={80} />
         <SkillBar skill="Chakra UI" percentage={90} />
-        <SkillBar skill="Strapi" percentage={90} />
+        <SkillBar skill="Bootstrap" percentage={90} />
+        <SkillBar skill="Headless CMS" percentage={90} />
         <SkillBar skill="Mongo DB" percentage={90} />
       </div>
     </main>
